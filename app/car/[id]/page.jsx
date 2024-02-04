3fd64ce043622cc48b08d7466b2983ccb1607b97
Ex10101 <sergeydavidovich12@gmail.com>
2024-02-04 22:21:28 +0100

    car show page start

--- app/car/[id]/page.jsx
+++ app/car/[id]/page.jsx
@@ -1,15 +1,32 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
-function CarPage() {
+function CarPage({params}) {
+
+    const carId = params.id
+    const [car, setCar] = useState([])
 
     useEffect(() => {
+        const fetchCar = async () => {
+            const response = await fetch(`/api/car/${carId}`)
+            const data = await response.json()
 
-    }, [])
+            setCar(data);
+        }
+        if(params?.id) fetchCar();
+    }, [params.id])
     
     return (
         <div>
-            YA GAY
+            <section>
+                <div className='red-color w-full h-[435px]'>
+                </div>
+                <div className='red-color w-full h-[13px] mt-1'>
+                </div>
+                <div className='red-color w-full h-[23px] mt-2'>
+                </div>
+                <h1>{car.name}</h1>
+            </section>
         </div>
     )
 }
